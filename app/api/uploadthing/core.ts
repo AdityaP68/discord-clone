8fce0085503ecc9923778ad14439ee55d1a562fd
Aditya Prakash <adityaprakash6688@gmail.com>
2023-09-22 03:57:06 +0530

    upload feature added

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,24 @@
+import { auth } from "@clerk/nextjs";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+
+const handleAuth = () => {
+  const userId = auth();
+  if (!userId) throw new Error("Unauthorized");
+  return { userId: userId };
+};
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {
+  // Defining file route
+  serverImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    // Set permissions and file types for this FileRoute
+    .middleware(() => handleAuth())
+    // This code RUNS ON YOUR SERVER after upload on usccf
+    .onUploadComplete(() => {}),
+  messageFile: f(["image", "pdf"])
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
